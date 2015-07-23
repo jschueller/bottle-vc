2bc558383ed70cde3bdc5a21058ed7691bff1e23
Julien Schueller <schueller@phimeca.com>
2015-07-23 11:35:08 +0200

    10s mspdbsrv timeout

--- vc-cmake.bat
+++ vc-cmake.bat
@@ -7,4 +7,4 @@ set CC=
 set CXX=
 
 rem FIXME: https://bugs.winehq.org/show_bug.cgi?id=38859
-start mspdbsrv.exe -start -spawn -shutdowntime -1
+start mspdbsrv.exe -verbose -start -spawn -shutdowntime 10
