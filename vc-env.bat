e8adf5fb845f69fc4327a3b072a4b0587e369588
Julien Schueller <julien.schueller@gmail.com>
2016-05-01 11:57:44 +0200

    use reg query in wine 1.9.9

--- vc-env.bat
+++ vc-env.bat
@@ -1,20 +1,20 @@
 echo @off
 
-rem FIXME: "%VSSHELL2010INSTALLDIR%VC\bin\vcvars32.bat
-rem fails: reg query "HKLM\Software\Microsoft\VisualStudio\SxS\VS7" /v "10.0"
-rem https://bugs.winehq.org/show_bug.cgi?id=24017
-
+rem needs reg query from wine 1.9.9
 set VSSHELL2010INSTALLDIR=C:\Program Files\Microsoft Visual Studio 10.0\
-set VS100COMNTOOLS=%VSSHELL2010INSTALLDIR%\Common7\Tools\
-set VS100COMNTOOLSDIR=%VS100COMNTOOLS%
-set VSINSTALLDIR=%VSSHELL2010INSTALLDIR%
-set WindowsSdkDir=C:\Program Files\Microsoft SDKs\Windows\v7.0A\
-set DevEnvDir=%VSINSTALLDIR%Common7\IDE\
-set INCLUDE=%VSINSTALLDIR%VC\INCLUDE;%INCLUDE%
-set INCLUDE=%WindowsSdkDir%\Include;%INCLUDE%
-set LIB=%VSINSTALLDIR%VC\LIB;%LIB%
-set LIB=%WindowsSdkDir%Lib;%LIB%
-set PATH=%VSINSTALLDIR%Common7\Tools;%PATH%
-set PATH=%VSINSTALLDIR%VC\bin;%PATH%
-set PATH=%DevEnvDir%;%PATH%
-set PATH=%WindowsSdkDir%bin;%PATH%
+call "%VSSHELL2010INSTALLDIR%VC\bin\vcvars32.bat"
+
+rem else use this:
+rem set VS100COMNTOOLS=%VSSHELL2010INSTALLDIR%\Common7\Tools\
+rem set VS100COMNTOOLSDIR=%VS100COMNTOOLS%
+rem set VSINSTALLDIR=%VSSHELL2010INSTALLDIR%
+rem set WindowsSdkDir=C:\Program Files\Microsoft SDKs\Windows\v7.0A\
+rem set DevEnvDir=%VSINSTALLDIR%Common7\IDE\
+rem set INCLUDE=%VSINSTALLDIR%VC\INCLUDE;%INCLUDE%
+rem set INCLUDE=%WindowsSdkDir%\Include;%INCLUDE%
+rem set LIB=%VSINSTALLDIR%VC\LIB;%LIB%
+rem set LIB=%WindowsSdkDir%Lib;%LIB%
+rem set PATH=%VSINSTALLDIR%Common7\Tools;%PATH%
+rem set PATH=%VSINSTALLDIR%VC\bin;%PATH%
+rem set PATH=%DevEnvDir%;%PATH%
+rem set PATH=%WindowsSdkDir%bin;%PATH%
